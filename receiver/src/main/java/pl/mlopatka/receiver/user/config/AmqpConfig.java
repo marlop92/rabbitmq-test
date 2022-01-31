74dc123ea1246149551cc7b20ea6cb12c9832b5d
marcinlopatka <lopatkamarcin@o2.pl>
2022-01-31 21:57:54 +0100

    Use RabbitListener instead of Bean Configuration

--- receiver/src/main/java/pl/mlopatka/receiver/user/config/AmqpConfig.java
+++ receiver/src/main/java/pl/mlopatka/receiver/user/config/AmqpConfig.java
@@ -5,13 +5,8 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import pl.mlopatka.receiver.user.UserReceiver;
 
 @Configuration
 @RequiredArgsConstructor
